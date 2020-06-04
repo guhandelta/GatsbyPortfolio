fa54222fc8bac4de8582889cb22ca8ce3639214d
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-04 00:52:29 -0400

    minors corrections

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
-import Header from './Header'
-import Image from './Image'
+import Header from './header'
 import Banner from './Banner'
 import Title from './Title'
 import Welcome from './Welcome'
 import Exp from './Exp'
 
-export { Header, Image, Banner, Exp, Title, Welcome }
+export { Header, Banner, Exp, Title, Welcome }
